645fcdc93ab5c17d652a00359213ce1b4eb1fb65
Vladimir <gahfag@mail.ru>
2024-02-25 20:17:49 +0300

    Create React Web App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  // Функция для обработки данных после аутентификации
+  const handleTelegramResponse = (response) => {
+    // Здесь вы можете добавить логику для сохранения данных пользователя
+    setUser(response);
+  };
+
+  window.TelegramLoginWidget = {
+    dataOnauth: (user) => handleTelegramResponse(user)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          {user ? (
+              <p>Привет, {user.first_name}! Вы успешно аутентифицированы.</p>
+          ) : (
+              <p>Привет, это мой Telegram Mini App!</p>
+          )}
+          <p>Текущее время: {currentTime}</p>
+          {!user && <div id="telegram-login-your_bot_username" data-onauth="handleTelegramResponse(user)"></div>}
+        </header>
+      </div>
   );
 }
 
